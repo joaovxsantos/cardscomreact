9496c2c2d39c90e63e79fc11bef971aa74d9492d
João Vitor <joaovitor55670@gmail.com>
2024-05-28 16:18:20 -0300

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,56 @@
 import './App.css'
 import { Card } from './assets/Card'
 import { DATA_TEXTS } from './assets/data'
+import React from 'react'
 
 function App() {
   return (
     <>
-      <div className="card-container">
-        {/* <Card {...DATA_TEXTS[0]} />
-        <Card {...DATA_TEXTS[1]} />
-        <Card {...DATA_TEXTS[2]} /> */}
-        {/* <Card name={DATA_TEXTS[0].name} title={DATA_TEXTS[0].title} image={DATA_TEXTS[0].image} position={DATA_TEXTS[0].position} />
-        <Card name={DATA_TEXTS[1].name} title={DATA_TEXTS[1].title} image={DATA_TEXTS[1].image} position={DATA_TEXTS[1].position} />
-        <Card name={DATA_TEXTS[2].name} title={DATA_TEXTS[2].title} image={DATA_TEXTS[2].image} position={DATA_TEXTS[2].position} /> */}
-        {DATA_TEXTS.map((data) => <Card {...data} />)}
-      </div>
+      {/* {React.createElement(
+        "div",
+        { className: "card-container" },
+        DATA_TEXTS.map((data) => <Card {...data} />)
+      )} */}
+      {React.createElement(
+        'div',
+        { className: 'card-container' },
+        React.createElement(
+          'div',
+          { className: 'card' },
+          React.createElement(
+            'div',
+            { className: 'icon' },
+            React.createElement('img', { src: DATA_TEXTS[0].image })
+          ),
+          React.createElement('h3', null, [DATA_TEXTS[0].name]),
+          React.createElement('p', null, [DATA_TEXTS[0].title]),
+          React.createElement('a', { id: DATA_TEXTS[0].position }, 'Saiba mais')
+        ),
+        React.createElement(
+          'div',
+          { className: 'card' },
+          React.createElement(
+            'div',
+            { className: 'icon' },
+            React.createElement('img', { src: DATA_TEXTS[1].image })
+          ),
+          React.createElement('h3', null, [DATA_TEXTS[1].name]),
+          React.createElement('p', null, [DATA_TEXTS[1].title]),
+          React.createElement('a', { id: DATA_TEXTS[1].position }, 'Saiba mais')
+        ),
+        React.createElement(
+          'div',
+          { className: 'card' },
+          React.createElement(
+            'div',
+            { className: 'icon' },
+            React.createElement('img', { src: DATA_TEXTS[2].image })
+          ),
+          React.createElement('h3', null, [DATA_TEXTS[2].name]),
+          React.createElement('p', null, [DATA_TEXTS[2].title]),
+          React.createElement('a', { id: DATA_TEXTS[2].position }, 'Saiba mais')
+        ),
+      )}
     </>
   )
 }
